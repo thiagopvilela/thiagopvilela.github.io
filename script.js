cab1207b935c721a7864261affcefec46a6b0f3a
thiagopvilela <158522554+thiagopvilela@users.noreply.github.com>
2024-06-19 20:32:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,142 @@
+document.getElementById('mobileMenuBtn').addEventListener('click', function() {
+    document.querySelector('.sideHeader').style.display = 'flex';
+});
+
+document.querySelector('.sideHeaderBtn').addEventListener('click', function() {
+    const sideHeader = document.querySelector('.sideHeader');
+    if (sideHeader.style.display === 'none') {
+      sideHeader.style.display = 'block';
+    } else {
+      sideHeader.style.display = 'none';
+    }
+  });
+
+
+
+
+
+document.getElementById('pb1').addEventListener('click', function() {
+  var content = document.getElementById('detail1');
+  if (content.style.display === 'none' || content.style.display === '') {
+      content.style.display = 'flex';
+  } else {
+      content.style.display = 'none';
+  }
+});
+
+document.getElementById('rdbc1').addEventListener('click', function() {
+  var content = document.getElementById('detail1');
+  if (content.style.display === 'none' || content.style.display === '') {
+      content.style.display = 'flex';
+  } else {
+      content.style.display = 'none';
+  }
+});
+
+
+
+document.getElementById('pb2').addEventListener('click', function() {
+  var content = document.getElementById('detail2');
+  if (content.style.display === 'none' || content.style.display === '') {
+      content.style.display = 'flex';
+  } else {
+      content.style.display = 'none';
+  }
+});
+
+document.getElementById('rdbc2').addEventListener('click', function() {
+  var content = document.getElementById('detail2');
+  if (content.style.display === 'none' || content.style.display === '') {
+      content.style.display = 'flex';
+  } else {
+      content.style.display = 'none';
+  }
+});
+
+
+
+
+document.getElementById('pb3').addEventListener('click', function() {
+  var content = document.getElementById('detail3');
+  if (content.style.display === 'none' || content.style.display === '') {
+      content.style.display = 'flex';
+  } else {
+      content.style.display = 'none';
+  }
+});
+
+document.getElementById('rdbc3').addEventListener('click', function() {
+  var content = document.getElementById('detail3');
+  if (content.style.display === 'none' || content.style.display === '') {
+      content.style.display = 'flex';
+  } else {
+      content.style.display = 'none';
+  }
+});
+
+
+
+
+
+document.getElementById('pb4').addEventListener('click', function() {
+  var content = document.getElementById('detail4');
+  if (content.style.display === 'none' || content.style.display === '') {
+      content.style.display = 'flex';
+  } else {
+      content.style.display = 'none';
+  }
+});
+
+document.getElementById('rdbc4').addEventListener('click', function() {
+  var content = document.getElementById('detail4');
+  if (content.style.display === 'none' || content.style.display === '') {
+      content.style.display = 'flex';
+  } else {
+      content.style.display = 'none';
+  }
+});
+
+
+
+
+
+document.getElementById('pb5').addEventListener('click', function() {
+  var content = document.getElementById('detail5');
+  if (content.style.display === 'none' || content.style.display === '') {
+      content.style.display = 'flex';
+  } else {
+      content.style.display = 'none';
+  }
+});
+
+document.getElementById('rdbc5').addEventListener('click', function() {
+  var content = document.getElementById('detail5');
+  if (content.style.display === 'none' || content.style.display === '') {
+      content.style.display = 'flex';
+  } else {
+      content.style.display = 'none';
+  }
+});
+
+
+
+
+
+
+document.getElementById('pb6').addEventListener('click', function() {
+  var content = document.getElementById('detail6');
+  if (content.style.display === 'none' || content.style.display === '') {
+      content.style.display = 'flex';
+  } else {
+      content.style.display = 'none';
+  }
+});
+
+document.getElementById('rdbc6').addEventListener('click', function() {
+  var content = document.getElementById('detail6');
+  if (content.style.display === 'none' || content.style.display === '') {
+      content.style.display = 'flex';
+  } else {
+      content.style.display = 'none';
+  }
+});
